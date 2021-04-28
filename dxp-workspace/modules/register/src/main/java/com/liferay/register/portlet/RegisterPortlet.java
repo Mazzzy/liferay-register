f7675e304f61e5eb6b6a802a5fd2671107062b12
Mazahar Shaikh <mazahar.shaikh@bfhl.in>
2021-04-28 16:40:03 +0530

    Resolved service dependency, called services and saved registration details in mysql db

--- dxp-workspace/modules/register/src/main/java/com/liferay/register/portlet/RegisterPortlet.java
+++ dxp-workspace/modules/register/src/main/java/com/liferay/register/portlet/RegisterPortlet.java
@@ -1,10 +1,17 @@
 package com.liferay.register.portlet;
 
 import com.liferay.register.constants.RegisterPortletKeys;
+import com.liferay.register.services.model.Register;
+import com.liferay.register.services.service.RegisterLocalService;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
+import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.search.ParseException;
 import com.liferay.portal.kernel.util.ParamUtil;
 
 import javax.portlet.Portlet;
@@ -13,6 +20,7 @@ import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author mazaharshaikh
@@ -33,6 +41,19 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class RegisterPortlet extends MVCPortlet {
+	@Reference
+	private RegisterLocalService _registerLocalService;
+	
+	private Date StringtoDate(String date) throws Exception {
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+        java.sql.Date sqlDate = null;
+        if( !date.isEmpty()) {
+
+            java.util.Date normalDate = sdf1.parse(date);
+			sqlDate = new java.sql.Date(normalDate.getTime());
+        }
+        return sqlDate;
+    }
 	
 	public void registerSubmit(ActionRequest request, ActionResponse response) 
 	 throws IOException, PortletException {
@@ -41,6 +62,18 @@ public class RegisterPortlet extends MVCPortlet {
 		String bdate = ParamUtil.getString(request, "bdate");
 		String email = ParamUtil.getString(request, "email");
 		
-		System.out.println("=== Passed "+name+" : "+surname+" : "+bdate+" : "+email);
+		// db call
+		Register register = _registerLocalService.createRegister(CounterLocalServiceUtil.increment());
+		register.setName(name);
+		register.setSurname(surname);
+		try {
+			register.setBdate(StringtoDate(bdate));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		register.setEmail(email);
+		_registerLocalService.addRegister(register);
+		
+		System.out.println("=== Succesfully Added to DB "+name+" : "+surname+" : "+bdate+" : "+email);
 	}
 }
